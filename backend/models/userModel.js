6ddd570b1e256a1c5746d215286a52b11a3ed30c
Tyler washington <twashing@hamilton.edu>
2022-06-19 17:23:28 -0400

    Complete crud on backend with Authentication and Authorization

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+const mongooose = require("mongoose");
+
+const userSchema = mongooose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongooose.model("User", userSchema);
