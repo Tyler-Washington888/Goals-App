405f5201de362a1bff6e86fd176e7a599f9e2c13
Tyler washington <twashing@hamilton.edu>
2022-06-20 15:46:37 -0400

    created header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">GoalSetter</Link>
+        <Link to="/">GoalSette</Link>
       </div>
       <ul>
         <li>
