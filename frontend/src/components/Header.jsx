3b49a0a16ebc6206bd79dda1be09baea96c56273
Tyler washington <twashing@hamilton.edu>
2022-06-20 15:47:33 -0400

    fixed typo in header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">GoalSette</Link>
+        <Link to="/">GoalSetter</Link>
       </div>
       <ul>
         <li>
