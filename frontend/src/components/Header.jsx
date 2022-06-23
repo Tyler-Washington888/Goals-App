2712678ad1e3565d64b4cf49655aecbe1f3a3902
Tyler washington <twashing@hamilton.edu>
2022-06-23 12:07:42 -0400

    create routes in app.js

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">GoalSetter</Link>
+        <Link to="/">Goalsetter</Link>
       </div>
       <ul>
         <li>
